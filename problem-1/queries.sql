93f32d453c46a0ef5c8c5992d6917b00c0f40454
campbell-kramer <165936293+campbell-kramer@users.noreply.github.com>
2025-06-10 01:11:15 -0230

    add queries to input data into problem-1 tables

--- problem-1/queries.sql
+++ problem-1/queries.sql
@@ -31,3 +31,33 @@ CREATE TABLE enrollments (
 	enrollment_date DATE
 	PRIMARY KEY (student_id, course_id)
 );
+
+INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+('Ellen', 'Ripley', 'jonesy92@weylandyutani.com', '2120-06-03'),
+('Arthur', 'Dallas', 'cpt_dallas@weylandyutani.com', '2120-06-03'),
+('Joan', 'Lambert', 'babygirl99@weylandyutani.com', '2120-06-03'),
+('Thomas', 'Kane', 'gilbertkane@weylandyutani.com', '2120-06-03'),
+('Dennis', 'Parker', 'DParker80@weylandyutani.com', '2120-06-03');
+
+INSERT INTO departments (department_name) VALUES
+('Weyland-Yutani'),
+('Seegson'),
+('Walmart');
+
+INSERT INTO professors (first_name, last_name, department_id) VALUES
+('Peter', 'Weyland', 1),
+('Hideo', 'Yutani', 1),
+('Josiah', 'Sieg', 2),
+('Sam', 'Walton', 3);
+
+INSERT INTO courses (course_name, course_description, professor_id) VALUES
+('Space Mining 1001', 'Introductory course for space mining majors.', (SELECT professor_id FROM professors WHERE first_name = 'Hideo' AND last_name = 'Yutani')),
+('Company Etiquette', 'Immerses students in proper behaviour and conduct while working for a corporation.', (SELECT professor_id FROM professors WHERE first_name = 'Peter' AND last_name = 'Weyland')),
+('Deep Space Psychological Assessment', 'Trains and tests student ability to safely live in deep space for extended periods.', (SELECT professor_id FROM professors WHERE first_name = 'Josiah' AND last_name = 'Sieg'));
+
+INSERT INTO enrollments (student_id, course_id, enrollment_date) VALUES
+((SELECT student_id FROM students WHERE first_name = 'Ellen' AND last_name = 'Ripley'), (SELECT course_id FROM courses WHERE course_name = 'Space Mining 1001'), '2120-08-18'),
+((SELECT student_id FROM students WHERE first_name = 'Joan' AND last_name = 'Lambert'), (SELECT course_id FROM courses WHERE course_name = 'Company Etiquette'), '2120-08-30'),
+((SELECT student_id FROM students WHERE first_name = 'Thomas' AND last_name = 'Kane'), (SELECT course_id FROM courses WHERE course_name = 'Deep Space Psychological Assessment'), '2120-09-01'),
+((SELECT student_id FROM students WHERE first_name = 'Arthur' AND last_name = 'Dallas'), (SELECT course_id FROM courses WHERE course_name = 'Company Etiquette'), '2120-07-29'),
+((SELECT student_id FROM students WHERE first_name = 'Dennis' AND last_name = 'Parker'), (SELECT course_id FROM courses WHERE course_name = 'Space Mining 1001'), '2120-08-25');
