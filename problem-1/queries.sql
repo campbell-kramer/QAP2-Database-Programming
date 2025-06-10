d2e6c34d71a4a460e49f715c529305287bc3e0db
campbell-kramer <165936293+campbell-kramer@users.noreply.github.com>
2025-06-10 01:08:39 -0230

    fix code formatting in problem-1 queries.sql

--- problem-1/queries.sql
+++ problem-1/queries.sql
@@ -1,14 +1,14 @@
 CREATE TABLE students (
-    student_id SERIAL PRIMARY KEY,
-    first_name TEXT,
-    last_name TEXT,
-    email TEXT,
-    enrollment_date DATE
+	student_id SERIAL PRIMARY KEY,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  enrollment_date DATE
 );
 
 CREATE TABLE departments (
-    department_id SERIAL PRIMARY KEY,
-    department_name TEXT
+	department_id SERIAL PRIMARY KEY,
+	department_name TEXT
 );
 
 CREATE TABLE professors (
@@ -19,15 +19,15 @@ CREATE TABLE professors (
 )
 
 CREATE TABLE courses (
-    course_id SERIAL PRIMARY KEY,
-    course_name TEXT,
-    course_description TEXT,
+	course_id SERIAL PRIMARY KEY,
+	course_name TEXT,
+	course_description TEXT,
 	professor_id REFERENCES professors(professor_id)
 );
 
 CREATE TABLE enrollments (
-    student_id INT REFERENCES students(student_id),
-    course_id INT REFERENCES courses(course_id),
-    enrollment_date DATE
+	student_id INT REFERENCES students(student_id),
+	course_id INT REFERENCES courses(course_id),
+	enrollment_date DATE
 	PRIMARY KEY (student_id, course_id)
 );
