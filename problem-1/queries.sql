8c7c5dc1e0e5e6dabdbbbe558abbe9e8401062cd
campbell-kramer <165936293+campbell-kramer@users.noreply.github.com>
2025-06-10 03:34:46 -0230

    bug fixes for problem-1

--- problem-1/queries.sql
+++ problem-1/queries.sql
@@ -16,19 +16,19 @@ CREATE TABLE professors (
 	first_name TEXT,
 	last_name TEXT,
 	department_id INT REFERENCES departments(department_id)
-)
+);
 
 CREATE TABLE courses (
 	course_id SERIAL PRIMARY KEY,
 	course_name TEXT,
 	course_description TEXT,
-	professor_id REFERENCES professors(professor_id)
+	professor_id INT REFERENCES professors(professor_id)
 );
 
 CREATE TABLE enrollments (
 	student_id INT REFERENCES students(student_id),
 	course_id INT REFERENCES courses(course_id),
-	enrollment_date DATE
+	enrollment_date DATE,
 	PRIMARY KEY (student_id, course_id)
 );
 
